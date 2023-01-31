477da64b9f8c99a7a586ce772ba2d3138b10e20d
Marcin Torba <torbamarcin@gmail.com>
2023-01-31 02:02:18 +0100

    Move to nano DI. (#2)
    * Move to nano DI.
    
    * Update
    
    * Add missing logger.

---
+++ Tests/MakoIoT.Device.Services.Server.Test/Mocks/MockLogger.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Reflection;
+
+namespace MakoIoT.Device.Services.Server.Test.Mocks
+{
+    internal class MockLogger : ILogger
+    {
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Log(LogLevel logLevel, EventId eventId, string state, Exception exception, MethodInfo format)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
